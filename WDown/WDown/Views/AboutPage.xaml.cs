fb1a49b5980985313234f9b855a56f45ae6de3b7
Mike Koenig <mikekoenig@live.com>
2019-02-12 11:56:39 -0800

    Add project files.

---
+++ WDown/WDown/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WDown.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
